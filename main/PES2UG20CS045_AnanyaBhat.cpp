68b7b46f2743fabc9d0e9d218a5760434d2eeb33
Ananya Bhat <85639761+Ananya207@users.noreply.github.com>
2023-02-18 20:33:22 +0530

    Add files via upload

---
+++ main/PES2UG20CS045_AnanyaBhat.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "SRN : PES2UG20CS045, NAME: Ananya Bhat \n";
+  cout << " Executing Task4 to see if job is updated automatically \n";
+  return 0;
+}
